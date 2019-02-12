fa254a7cbef22fbf2c068dae9a6456c758d4d9c3
KDragon <elkarouani@gmail.com>
2019-02-12 11:32:47 +0100

    Task: Initialize Firebase

--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+var config = {
+    apiKey: "AIzaSyCy3IwLp7tPoRjiZi3B5UxfQu_YJWBwwRs",
+    authDomain: "real-time-chat-database.firebaseapp.com",
+    databaseURL: "https://real-time-chat-database.firebaseio.com",
+    projectId: "real-time-chat-database",
+    storageBucket: "real-time-chat-database.appspot.com",
+    messagingSenderId: "545291923833"
+ };
+ firebase.initializeApp(config);
+
+
 Vue.config.productionTip = false
 
 new Vue({
