bdb4e68e313d26f65cf5d077aaa3b4d24e751e2b
KDragon <elkarouani@gmail.com>
2019-02-16 19:51:29 +0100

    Task: adding displaying messages from database fonctionality

--- src/views/PrivateChat.vue
+++ src/views/PrivateChat.vue
@@ -92,48 +92,15 @@
         	</div>
         	<div class="mesgs">
           	<div class="msg_history">
-            	<div class="incoming_msg">
+            	<div v-for="message in messages" class="incoming_msg">
               	<div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
               	<div class="received_msg">
                 	<div class="received_withd_msg">
-                  	<p>Test which is a new approach to have all
-                    	solutions</p>
+                  	<p>{{message.message}}</p>
                   	<span class="time_date"> 11:01 AM    |    June 9</span>
                   </div>
               	</div>
             	</div>
-            	<div class="outgoing_msg">
-              	<div class="sent_msg">
-                	<p>Test which is a new approach to have all
-                  	solutions</p>
-                	<span class="time_date"> 11:01 AM    |    June 9</span> 
-                </div>
-            	</div>
-            	<div class="incoming_msg">
-              	<div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-              	<div class="received_msg">
-                	<div class="received_withd_msg">
-                  	<p>Test, which is a new approach to have</p>
-                  	<span class="time_date"> 11:01 AM    |    Yesterday</span>
-                  </div>
-              	</div>
-            	</div>
-            	<div class="outgoing_msg">
-              	<div class="sent_msg">
-                	<p>Apollo University, Delhi, India Test</p>
-                	<span class="time_date"> 11:01 AM    |    Today</span> 
-                </div>
-            	</div>
-            	<div class="incoming_msg">
-              	<div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-              	<div class="received_msg">
-                	<div class="received_withd_msg">
-                  	<p>We work directly with our designers and suppliers,
-                    	and sell direct to you, which means quality, exclusive
-                    	products, at a price anyone can afford.</p>
-                  	<span class="time_date"> 11:01 AM    |    Today</span>
-                </div>
-              </div>
             </div>
           </div>
           <div class="type_msg">
@@ -153,7 +120,8 @@
 export default {
   data() {
     return {
-      message: null
+      message: null,
+      messages: []
     }
   },
   
@@ -161,7 +129,6 @@ export default {
     saveMessage(){
       db.collection('chat').add({
         message: this.message,
-        messages: [],
       }).then((docRef)=>{
         console.log("Document written with ID: ", docRef.id);
       }).catch((error)=>{
@@ -176,14 +143,13 @@ export default {
         querySnapshot.forEach(doc=>{
           allMessages.push(doc.data());
         });
-
         this.messages = allMessages;
       })
-    },
+    }    
+  },
 
-    created() {
-      this.fetchMessages();
-    }
+  created() {
+    this.fetchMessages();
   }
 }
 </script>
