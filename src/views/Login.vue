25eea30b78be1cbe020aea1efbfcc3fb72465a31
KDragon <elkarouani@gmail.com>
2019-02-17 16:56:55 +0100

    Task: adding route for login

---
+++ src/views/Login.vue
@@ -0,0 +1,38 @@
+<template>
+	<div>
+		<h3>PLease Login with your google account to continue</h3>
+
+		<button @click="login">Login with google</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		import firebase from 'firebase';
+		methods:{
+			login(){
+				var provider = new firebase.auth.GoogleAuthProvider();
+				provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+
+				firebase.auth().getRedirectResult().then((result) => {
+				  	if (result.credential) {
+				    	// This gives you a Google Access Token. You can use it to access the Google API.
+				    	var token = result.credential.accessToken;
+				    	// ...
+				  	}
+				  	// The signed-in user info.
+				 	var user = result.user;
+				}).catch(function(error) {
+				 	// Handle Errors here.
+				  	var errorCode = error.code;
+				  	var errorMessage = error.message;
+				  	// The email of the user's account used.
+				  	var email = error.email;
+				  	// The firebase.auth.AuthCredential type that was used.
+				  	var credential = error.credential;
+				  	// ...
+				});
+			}
+		}
+	}
+</script>
